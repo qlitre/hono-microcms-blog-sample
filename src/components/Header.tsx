4f3462154b5cc10f5115584f58a2111311ec813b
qlitre <kuri0403@gmail.com>
2023-10-19 22:17:42 +0900

    update repo URL

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@
 export const Header = () => {
     return (
         <header class="header">
-            <h1 class="siteTitle">Hono microCMS Blog Sample🔥</h1>
+            <h1 class="siteTitle">Hono🔥 microCMS Blog Sample</h1>
             <nav class="navigations">
-                <a class="navigationLink" href="https://github.com/qlitre">GitHub</a>
+                <a class="navigationLink" href="https://github.com/qlitre/hono-microcms-blog-sample">GitHub</a>
             </nav>
         </header>
     );
