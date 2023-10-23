c98781226abb04dd6ae99dc8d86725ded1cc4eac
qlitre <kuri0403@gmail.com>
2023-10-24 00:04:06 +0900

    add draft preview page

--- src/index.tsx
+++ src/index.tsx
@@ -18,6 +18,9 @@ const limit = BLOG_PER_PAGE
 app.use('/static/*', serveStatic({ root: './' }))
 app.use('/favicon.ico', serveStatic({ path: './favicon.ico' }))
 
+/**
+ * トップページ
+ */
 app.get('/', async (c) => {
     const client = createClient({
         serviceDomain: c.env.SERVICE_DOMAIN,
@@ -42,6 +45,9 @@ app.get('/', async (c) => {
     return c.html(<HomeContent {...props} />)
 })
 
+/**
+ * 記事詳細ページ
+ */
 app.get('/post/:slug', async (c) => {
     const slug = c.req.param('slug')
     const client = createClient({
@@ -62,6 +68,35 @@ app.get('/post/:slug', async (c) => {
     return c.html(<DetailContent {...props} />)
 })
 
+
+/**
+ * 下書き中の記事詳細ページ
+ */
+app.get('/post/:slug/draft', async (c) => {
+    const slug = c.req.param('slug')
+    const draftKey = c.req.query('draftKey')
+    const client = createClient({
+        serviceDomain: c.env.SERVICE_DOMAIN,
+        apiKey: c.env.API_KEY,
+    })
+    const listDetail = await client.getListDetail<Post>({ endpoint: 'post', contentId: slug, queries: { draftKey: draftKey } })
+    listDetail.title = listDetail.title + "（下書き）"
+    const props = {
+        post: listDetail,
+        siteData: {
+            title: listDetail.title,
+            description: listDetail.description,
+            ogpType: "article" as const,
+            ogpImage: listDetail.thumbnail?.url,
+            ogpUrl: config.siteURL + `/post/${slug}`,
+        },
+    }
+    return c.html(<DetailContent {...props} />)
+})
+
+/**
+ * ページ番号でアクセス
+ */
 app.get('/page/:pageId', async (c) => {
     const pageId = c.req.param('pageId')
     const client = createClient({
@@ -91,6 +126,9 @@ app.get('/page/:pageId', async (c) => {
 })
 
 
+/**
+ * カテゴリー指定でアクセス
+ */
 app.get('/:categoryId/page/:pageId', async (c) => {
     const categoryId = c.req.param('categoryId')
     const pageId = c.req.param('pageId')
@@ -129,6 +167,9 @@ app.get('/:categoryId/page/:pageId', async (c) => {
     return c.html(<HomeContent {...props} />)
 })
 
+/**
+ * タグ指定でアクセス
+ */
 app.get('/tags/:tagId/page/:pageId', async (c) => {
     const tagId = c.req.param('tagId')
     const pageId = c.req.param('pageId')
